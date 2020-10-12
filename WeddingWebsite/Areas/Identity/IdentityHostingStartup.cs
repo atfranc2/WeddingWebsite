4976928fbce01fb790d775e83c5a5f0a68b5d99d
atfranc2 <52332881+atfranc2@users.noreply.github.com>
2020-10-12 11:16:47 -0400

    Configured user authentication

---
+++ WeddingWebsite/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using WeddingWebsite.Data;
+
+[assembly: HostingStartup(typeof(WeddingWebsite.Areas.Identity.IdentityHostingStartup))]
+namespace WeddingWebsite.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
